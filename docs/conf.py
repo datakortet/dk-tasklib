c468f4552138cf89d503a47c8786a6de7ce8143d
Bjorn Pettersen <bp@norsktest.no>
2022-01-17 19:21:42 +0100

    tagged-upversion

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ copyright = u'2016 Bjorn Pettersen'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0.4'
+version = '3.0.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
